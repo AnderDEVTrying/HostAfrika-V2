4aedb890466296edb0d82c9e5acf88f9118fce05
Anderson Zango <azango94@gmail.com>
2024-07-15 08:58:23 +0200

    Adding login controller

--- src/main/java/com/HostAfrika/HostAfrika/Controller/AuthorizationController.java
+++ src/main/java/com/HostAfrika/HostAfrika/Controller/AuthorizationController.java
@@ -1,33 +1,67 @@
 package com.HostAfrika.HostAfrika.Controller;
 
+import com.HostAfrika.HostAfrika.DTO.UserDTO.ApiResponseDTO;
+import com.HostAfrika.HostAfrika.DTO.UserDTO.LoginRequestDTO;
+import com.HostAfrika.HostAfrika.DTO.UserDTO.LoginResponseDTO;
 import com.HostAfrika.HostAfrika.DTO.UserDTO.RegisterRequestDTO;
+import com.HostAfrika.HostAfrika.DTO.UserDTO.UserResponseDTO;
 import com.HostAfrika.HostAfrika.Domain.User.User;
+import com.HostAfrika.HostAfrika.Infra.Security.TokenService;
 import com.HostAfrika.HostAfrika.Repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("auth")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class AuthorizationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
     @Autowired
     private UserRepository repository;
 
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseDTO> login(@RequestBody @Valid LoginRequestDTO data){
+        try{
+            var emailPassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+            var auth = this.authenticationManager.authenticate(emailPassword);
+
+            var token = this.tokenService.generateToken((User) auth.getPrincipal());
+            return ResponseEntity.ok(new ApiResponseDTO(true, "Login Successful,",new LoginResponseDTO(token)));
+        }catch (RuntimeException exception){
+            return ResponseEntity.badRequest().body(new ApiResponseDTO(false, "Credentials do not match"));
+        }
+
+
+
+    }
+
+
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody @Valid RegisterRequestDTO data){
-        if(this.repository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+    public ResponseEntity<ApiResponseDTO> register(@RequestBody @Valid RegisterRequestDTO data){
+        if(this.repository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().body(
+                new ApiResponseDTO(false, "Email Already exists"));
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
         User newUser = new User(data.firstName(), data.lastName(),data.email(), encryptedPassword,
-                data.phoneNumber(), data.birthDate(), data.role());
+                data.phoneNumber(), data.birthDate());
 
         this.repository.save(newUser);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new ApiResponseDTO(true, "Registration Successful"));
+    }
+
+    @GetMapping("/users")
+    public List<UserResponseDTO> getUsers(){
+        return repository.findAll().stream().map(UserResponseDTO::new).toList();
     }
 }
--- src/main/java/com/HostAfrika/HostAfrika/Infra/Security/SecurityConfiguration.java
+++ src/main/java/com/HostAfrika/HostAfrika/Infra/Security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.HostAfrika.HostAfrika.Infra.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -23,7 +27,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/hostAfrika/users").hasRole("ADMIN"))
+                        .requestMatchers(HttpMethod.GET, "/auth/users").hasRole("ADMIN"))
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     @Bean
